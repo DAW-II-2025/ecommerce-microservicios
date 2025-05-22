c576541cef7fa1c4feee1852c776f234ec2ce4ec
jhonatanQuiñones <jhon2226g@gmail.com>
2025-05-22 11:41:36 -0500

    creacion los microservicios modules

---
+++ ecommerce-hs/msvc-archivos/src/main/java/pe/edu/cibertec/MsvcArchivosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcArchivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcArchivosApplication.class, args);
+	}
+
+}
