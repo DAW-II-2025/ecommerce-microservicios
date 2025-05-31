d8857d8eedaa6f3544ccbe4510626a5bb01cf92f
AntonyTrejo21 <antony.trejo.burga@gmail.com>
2025-05-30 22:28:17 -0500

    Estructura msvc-admin para CRUD e interfaz de chat del bot

---
+++ ecommerce-hs/msvc-admin/src/main/java/pe/edu/cibertec/controller/chat/ChatController.java
@@ -0,0 +1,8 @@
+package pe.edu.cibertec.controller.chat;
+
+import org.springframework.stereotype.Controller;
+
+//estimado hans recuerda que aqui puro controller, no hay restController pq sino el thymeleaf no lee u.u
+@Controller
+public class ChatController {
+}
---
+++ ecommerce-hs/msvc-admin/src/main/java/pe/edu/cibertec/controller/producto/ProductoController.java
@@ -0,0 +1,7 @@
+package pe.edu.cibertec.controller.producto;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductoController {
+}
