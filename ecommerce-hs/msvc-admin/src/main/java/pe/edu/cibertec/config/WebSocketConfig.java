09e1522e8678e7b3bd6e0621561c7364049e9d5f
jhonatanQuiñones <jhon2226g@gmail.com>
2025-06-24 08:47:52 -0500

    websocket

--- ecommerce-hs/msvc-admin/src/main/java/pe/edu/cibertec/config/WebSocketConfig.java
+++ ecommerce-hs/msvc-admin/src/main/java/pe/edu/cibertec/config/WebSocketConfig.java
@@ -11,8 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	
 	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		
+	public void registerStompEndpoints(StompEndpointRegistry registry) {		
 		registry.addEndpoint("/chat-websocket").setAllowedOrigins("http://localhost:8084")
 		.withSockJS();
 	}
